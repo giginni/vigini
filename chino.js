fb2c0db495548079b95e8ebdd1839b0bb1b92586
Gina Manfredi <gnmanfredi@gmail.com>
2021-12-30 00:13:00 +0100

    var const y mensajes

--- chino.js
+++ chino.js
@@ -1,3 +1,23 @@
 var inacosa;
+const catorra = "fiu";
 inacosa = "legarra la gente";
+console.log(inacosa);
+inacosa = "chino no come esto cosa";
+console.log(inacosa);
+inacosa = 12;
+console.log(inacosa); // Ejemplo de comentario
+/*
+comentario
+muchas
+
+lineas
+*/
+console.log(catorra);
+
+
+
+inacosa = "12";
+console.log(inacosa);
+inacosa = true;
+inacosa = false;
 console.log(inacosa);
