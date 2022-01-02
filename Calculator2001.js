37b0f389ff4a8a4a7090e2a6db7dd9c36dd9cbe8
Gina Manfredi <gnmanfredi@gmail.com>
2022-01-02 16:21:21 +0100

    bucle for

--- Calculator2001.js
+++ Calculator2001.js
@@ -3,14 +3,24 @@ const prompt = require('prompt-sync')({sigint: true});//importar modulo para lee
 var operacion;
 var numero1,numero2;
 
+function sumarString(num1, num2) {
+return (parseInt(num1) + parseInt(num2));
+}
+
+
 console.log("Calculadora\n Elija la operacion a realizar\n 1-Suma\n 2-Resta\n 3-Multiplicacion\n 4-Division\n");
 operacion = prompt("");
+if (!(operacion === "1" || operacion === "2" || operacion ==="3" || operacion ==="4")) {
+console.log("Escriba un numero valido");
+operacion = prompt("");
+}
+
 console.log(operacion);
 if (operacion === "1") {
 numero1 = prompt("Escriba un numero: ");
 numero2 = prompt("escriba otro numero: ");
 console.log("El resultado es: ");
-console.log(parseInt(numero1)+parseInt(numero2));
+console.log(sumarString(numero1, numero2));
 
 } else if (operacion === "2") {
   numero1 = prompt("Escriba un numero: ");
---
+++ for1.js
@@ -0,0 +1,42 @@
+// i = i + 1
+// i++;  i--; i+=3 ; i-=4;
+
+for (var i = 0; i < 10; i++) {
+console.log(i);
+}
+console.log("\n\n\n\n");
+
+// Multiplos de 2 menores o iguales a 10
+for (var i = 2; i <= 10; i+=2) {
+console.log(i);
+}
+
+console.log("\n\n\n\n");
+// Multiplos de 2 menores a 10
+for (var i = 2; i < 10; i+=2) {
+console.log(i);
+}
+console.log("\n\n\n\n");
+// Primeros 12 multiplos de 3
+for (var i = 3; i <= 36; i+=3) {
+  console.log(i);
+}
+console.log("\n\n\n\n");
+
+// Otra forma de hacerlo
+for (var i = 1; i <= 12; i++) {
+  console.log(3*i);
+}
+console.log("\n\n\n\n");
+
+
+// Primeros 20 numeros impares
+for (var i = 0; i <= 20; i++) {
+  console.log(2*i+1);
+}
+
+console.log("\n\n\n\n");
+// Contar del 20 al 1 en orden descendente
+for (var i = 20; i >= 1; i--) {
+  console.log(i);
+}
